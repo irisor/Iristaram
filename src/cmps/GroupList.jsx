61adf33af2a76275dcf5ee8307c4b60e4be56c38
iriso1 <114158685+iriso1@users.noreply.github.com>
2024-06-30 23:03:30 +0200

    Board styling - style new group element

--- src/cmps/GroupList.jsx
+++ src/cmps/GroupList.jsx
@@ -10,12 +10,12 @@ export function GroupList({ onRemoveGroup, onUpdateTitle, onAddGroup }) {
 
 	return (
 		<section className="group-list" ref={insideRef}>
-				{board?.groups?.map(group =>
-					<GroupPreview key={group.id} group={group} onRemoveGroup={onRemoveGroup} onUpdateTitle={onUpdateTitle} />
-				)}
-				<div className="group-preview">
-					<CreateItem onAddItem={onAddGroup} initialBtnLbl='Add another list' addBtnLabel='Add list' placeholder='Enter list title...' insideRef={insideRef}/>
-				</div>
+			{board?.groups?.map(group =>
+				<GroupPreview key={group.id} group={group} onRemoveGroup={onRemoveGroup} onUpdateTitle={onUpdateTitle} />
+			)}
+
+			<CreateItem onAddItem={onAddGroup} initialBtnLbl='Add another list' addBtnLabel='Add list' placeholder='Enter list title...' insideRef={insideRef} />
+
 		</section >
 	)
 }
