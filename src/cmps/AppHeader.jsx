7fd3202d0df52b2da559ecdea74d9b9487b7a4a4
iriso1 <114158685+iriso1@users.noreply.github.com>
2024-07-01 01:08:58 +0200

    AppHeader styling - fix hover

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -5,8 +5,10 @@ export function AppHeader() {
     return (
         <header className="app-header">
             <nav className="app-header-nav">
-                <NavLink to="" aria-label="Back to home" className="logo"></NavLink>
-                <NavLink to="/boards">Boards</NavLink>
+                <NavLink to="" aria-label="Back to home" className="app-header-logo btn">
+                    <div className="logo-img"></div>
+                </NavLink>
+                <NavLink to="/boards" className="app-header-nav-link btn">Boards</NavLink>
             </nav>
         </header>
     )
