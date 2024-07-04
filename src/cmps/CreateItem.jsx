b82fd49eafd7a86922ba1b344ef1920e40e1b36d
iriso1 <114158685+iriso1@users.noreply.github.com>
2024-07-04 17:56:18 +0200

    createItem - use bacground element to catch clicks instead of addEventListener

--- src/cmps/CreateItem.jsx
+++ src/cmps/CreateItem.jsx
@@ -11,20 +11,6 @@ export function CreateItem({ onAddItem, initialBtnLbl = 'Add', addBtnLabel = 'Ad
 	const inputRef = useRef(null)
 	const insideRef = useRef(null)
 
-	useEffect(() => {
-		function handleClick(ev) {
-			if (insideRef.current && !insideRef.current.contains(ev.target)) {
-				if (inputRef?.current?.value?.length > 0) {
-					handleAddItem({ title: inputRef.current.value }, ev)
-				} else {
-					setItemData(null)
-				}
-			}
-		}
-		document.removeEventListener('click', handleClick)
-		document.addEventListener('click', handleClick)
-		return () => document.removeEventListener('click', handleClick)
-	}, [insideRef])
 
 	useEffect(() => {
 		inputRef.current?.focus()
@@ -41,17 +27,21 @@ export function CreateItem({ onAddItem, initialBtnLbl = 'Add', addBtnLabel = 'Ad
 	}
 
 	function handleAddItem(newItem, ev = null) {
-		console.log('handleAddItem', newItem)
+		
+		// console.log('handleAddItem', newItem)
 		ev?.preventDefault()
+		ev?.stopPropagation()
 
 		setItemData(null)
-		
-		if (groupId) {
-			// For tasks, include groupId
-			onAddItem(board._id, groupId, newItem)
-		} else {
-			// For groups, omit groupId
-			onAddItem(board._id, newItem)
+
+		if (newItem?.title !== '') {
+			if (groupId) {
+				// For tasks, include groupId
+				onAddItem(board._id, groupId, newItem)
+			} else {
+				// For groups, omit groupId
+				onAddItem(board._id, newItem)
+			}
 		}
 	}
 
@@ -63,25 +53,28 @@ export function CreateItem({ onAddItem, initialBtnLbl = 'Add', addBtnLabel = 'Ad
 	return (
 		<>
 			{itemData ? (
-				<form key="new-item" className="create-item edit" onSubmit={ev => handleAddItem(itemData, ev)} ref={insideRef}>
-					<textarea
-						type="text"
-						className="editable-title-input"
-						value={itemData.title}
-						onChange={ev => onChangeTitle(ev.target.value)}
-						onKeyDown={ev => ev.key === 'Enter' && handleAddItem(itemData)}
-						placeholder={placeholder}
-						ref={inputRef}
-					/>
-					<button className="btn new-item-save btn-blue" onClick={ev => handleAddItem(itemData, ev)}>
-						{addBtnLabel}
-					</button>
-					<button className="btn icon new-item-close" onClick={ev => onClose(ev)}>
-						<IconContext.Provider value={{ color: 'inherit' }}>
-							<AiOutlineClose />
-						</IconContext.Provider>
-					</button>
-				</form>
+				<>
+					<div className="create-item-outside" onClick={ev => { handleAddItem(itemData, ev) }}></div>
+					<form key="new-item" className="create-item edit" ref={insideRef}>
+						<textarea
+							type="text"
+							className="editable-title-input"
+							value={itemData.title}
+							onChange={ev => onChangeTitle(ev.target.value)}
+							onKeyDown={ev => ev.key === 'Enter' && handleAddItem(itemData)}
+							placeholder={placeholder}
+							ref={inputRef}
+						/>
+						<button className="btn new-item-save btn-blue" onClick={ev => handleAddItem(itemData, ev)}>
+							{addBtnLabel}
+						</button>
+						<button className="btn icon new-item-close" onClick={ev => onClose(ev)}>
+							<IconContext.Provider value={{ color: 'inherit' }}>
+								<AiOutlineClose />
+							</IconContext.Provider>
+						</button>
+					</form>
+				</>
 			) : (
 				<button className="btn create-item non-edit" onClick={ev => onAddEmptyItem(ev)}>
 					<div className="icon">
