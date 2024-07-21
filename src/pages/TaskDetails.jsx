65e4c51d292298de9887d8a83299c85b03403466
iriso1 <114158685+iriso1@users.noreply.github.com>
2024-07-21 23:39:22 +0200

    TaskDetails - basic structure + using Trello fonts for icons

--- src/pages/TaskDetails.jsx
+++ src/pages/TaskDetails.jsx
@@ -1,17 +1,36 @@
 import { Modal } from "../cmps/Modal"
 import { useNavigate, useParams } from "react-router"
 import { useModal } from "../customHooks/useModal"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import { useSelector } from "react-redux"
+import { EditableTitle } from "../cmps/EditableTitle"
+import { updateTask } from "../store/board.actions"
+import { showErrorMsg, showSuccessMsg } from "../services/event-bus.service"
+import { Icon } from "../cmps/Icon"
+import { CreateItem } from "../cmps/CreateItem"
 
 export function TaskDetails() {
     const { boardId, taskId } = useParams()
     const { isOpen, openModal, closeModal } = useModal()
+    const [task, setTask] = useState({})
     const navigate = useNavigate()
 
+    const [currentTask, groupId, groupTitle] = useSelector((storeState) => {
+        const board = storeState.boardModule.board;
+        const group = board.groups.find(group => group.tasks.some(t => t.id === taskId));
+        const task = group?.tasks.find(task => task.id === taskId);
+        return [task, group?.id, group?.title];
+    });
 
     useEffect(() => {
-        openModal()
-    }, [taskId])
+        if (!taskId) return
+        openModal();
+        loadTask();
+    }, [taskId]);
+
+    function loadTask() {
+        setTask(currentTask);
+    }
 
     function onClose(ev) {
         ev.preventDefault()
@@ -20,10 +39,145 @@ export function TaskDetails() {
         navigate("..")
     }
 
+    function onUpdateTaskTitle(newTitle) {
+        if (!newTitle) return
+        try {
+            const newTask = { ...task, title: newTitle }
+            updateTask(boardId, groupId, newTask)
+            showSuccessMsg(`Task title updated`)
+        } catch (err) {
+            showErrorMsg('Cannot update task title')
+        }
+    }
+
+    if (!task) return null
     return (
-        // <div className='task-details task-modal' ref={refs.setReference}>
         <Modal isOpen={isOpen} closeModal={ev => onClose(ev)} cmpClassName='task-details'>
-            TaskDetails
+
+            <button className='btn icon task-details-close' onClick={ev => onClose(ev)}>
+                <Icon name="close" />
+            </button>
+            <article className='task-details-content'>
+                <section className='task-details-header'>
+                    <div className="task-details-card-icon icon icon-lg icon-card"></div>
+                    <EditableTitle className="task-details-title" initialTitle={task.title} onUpdateTitle={title => onUpdateTaskTitle(title)} />
+                    <div className="task-details-in-list">In list <a href="#">{groupTitle}</a></div>
+                </section>
+                <section className='task-details-main'>
+                    <section className="task-details-data">
+                        <section className="task-details-members">
+                            <h3 className="task-details-members">Members</h3>
+                            <section className="task-details-member-list">
+                                <button className="btn"></button>
+                            </section>
+                            <CreateItem />
+                        </section>
+                        <section className="task-details-notifications">
+                            <h3 className="task-details-notifications-title">Notifications</h3>
+                            <button className="task-details-notifications-btn">
+                                <span className="btn icon icon-sm icon-subscribe"></span>
+                                <p>Watch</p>
+                            </button>
+                        </section>
+                    </section>
+                    <section className="task-details-description">
+                        <section className="task-details-description-header">
+                            <Icon name="description" />
+                            <h3>Description</h3>
+                            <button className="btn">Edit</button>
+                        </section>
+                        <p>{task.description}</p>
+                    </section>
+                    <section className="task-details-activity">
+                        <section className="task-details-activity-header">
+                            <span className="icon icon-lg icon-activity"></span>
+                            <h3>Activity</h3>
+                            <button className="btn">Hide Details</button>
+                        </section>
+                        <section className="task-details-activity-list">
+                            <CreateItem />
+                            <article className="task-details-activity-item">
+                                <span className="member"></span>
+                                <div className="task-details-activity-item-desc">some activity</div>
+                                <div className="task-details-activity-item-time">Jul 14, 2024, 9:33 PM</div>
+                            </article>
+                        </section>
+                    </section>
+                </section>
+                <section className="task-details-sidebar">
+                    <section className="task-details-sidebar-module">
+                        <h3 className="task-details-sidebar-module-title">Add to card</h3>
+                        <section className="task-details-sidebar-module-list">
+                            <button className="btn">
+                                <span className="icon icon-sm icon-member" />
+                                <p>Members</p>
+                            </button>
+                            <button className="btn">
+                                <span className="icon icon-sm icon-label" />
+                                <p>Labels</p>
+                            </button>
+                            <button className="btn">
+                                <Icon name="checklist" />
+                                <p>Checklist</p>
+                            </button>
+                            <button className="btn">
+                                <Icon name="clock" />
+                                <p>Dates</p>
+                            </button>
+                            <button className="btn">
+                                <Icon name="attachment" />
+                                <p>Attachment</p>
+                            </button>
+                            <button className="btn">
+                                <span className="icon icon-sm icon-card-cover" />
+                                <p>Cover</p>
+                            </button>
+                            <button className="btn">
+                                <Icon name="customField" />
+                                <p>Custom Fields</p>
+                            </button>
+                        </section>
+                    </section>
+                    <section className="task-details-sidebar-module">
+                        <h3 className="task-details-sidebar-module-title">Power-Ups</h3>
+                        <section className="task-details-sidebar-module-list">
+                            <CreateItem />
+                        </section>
+                    </section>
+                    <section className="task-details-sidebar-module">
+                        <h3 className="task-details-sidebar-module-title">Automation</h3>
+                        <button className="btn"><Icon name="information" /></button>
+                        <section className="task-details-sidebar-module-list">
+                            <CreateItem />
+                        </section>
+                    </section>
+                    <section className="task-details-sidebar-module">
+                        <h3 className="task-details-sidebar-module-title">Actions</h3>
+                        <section className="task-details-sidebar-module-list">
+                            <button className="btn">
+                                <span className="icon icon-sm icon-move" />
+                                <p>Move</p>
+                            </button>
+                            <button className="btn">
+                                <span className="icon icon-sm icon-copy" />
+                                <p>Copy</p>
+                            </button>
+                            <button className="btn">
+                                <span className="icon icon-sm icon-template-card" />
+                                <p>Make template</p>
+                            </button>
+                            <button className="btn">
+                                <span className="icon icon-sm icon-archive" />
+                                <p>Archive</p>
+                            </button>
+                            <button className="btn">
+                                <Icon name="share" />
+                                <p>Share</p>
+                            </button>
+                        </section>
+                    </section>
+                </section>
+            </article>
         </Modal>
     )
 }
