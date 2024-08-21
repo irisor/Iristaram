4a7aeb0473a09b4bccfa231ce887378853defbc0
iriso1 <114158685+iriso1@users.noreply.github.com>
2024-08-21 15:59:06 +0200

    Labels popover - add to taskPreview

--- src/cmps/task/TaskPreview.jsx
+++ src/cmps/task/TaskPreview.jsx
@@ -3,11 +3,20 @@ import { Link, useParams } from "react-router-dom";
 export function TaskPreview({ task }) {
     const { boardId } = useParams()
 
+    function toggleLabelCollapse(ev) {
+        ev.stopPropagation()
+        ev.preventDefault()
+        ev.currentTarget.closest(".task-preview-labels").classList.toggle("collapsed")
+    }
+
     return (
         <div className="task-preview">
             <Link to={`/boards/${boardId}/${task.id}`}>
                 {task.cover?.url && <img className="task-preview-cover" src={task.cover?.url} alt="cover" />}
                 <div className="task-preview-container">
+                    <div className="task-preview-labels collapsed">
+                        {task.labels?.map(label => <div key={label.id} className="task-preview-label" style={{ backgroundColor: label.color }} onClick={ev => toggleLabelCollapse(ev)}><div className="task-preview-label-title">{label.title}</div></div>)}
+                    </div>
                     <p>{task.title}</p>
                 </div>
             </Link>
