747505fc5e53810e4910d117eac06f8f36b70443
iriso1 <114158685+iriso1@users.noreply.github.com>
2024-08-23 01:05:41 +0200

    Checklist - hide checked  items

--- src/cmps/task/TaskDetailsChecklistItemList.jsx
+++ src/cmps/task/TaskDetailsChecklistItemList.jsx
@@ -1,18 +1,18 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useForm } from "../../customHooks/useForm";
 import { TaskDetailsChecklistItemPreview } from "./TaskDetailsChecklistItemPreview";
 import { updateTask } from "../../store/board/board.actions";
 import { shallowEqual, useSelector } from "react-redux";
 import { useParams } from "react-router";
 
-export function TaskDetailsChecklistItemList({ checklist, onChangeProgress }) {
-    const { taskId } = useParams();
+export function TaskDetailsChecklistItemList({ checklist, onChangeProgress, hideCompleted }) {
+	const { taskId } = useParams();
 	const memoizedSelector = (storeState) => {
-        const board = storeState.boardModule.board
-        const group = board.groups.find(g => g.tasks.some(t => t.id === taskId))
-        const task = group?.tasks.find(t => t.id === taskId)
-        return { board, currentTask: task, groupId: group?.id }
-    }
+		const board = storeState.boardModule.board
+		const group = board.groups.find(g => g.tasks.some(t => t.id === taskId))
+		const task = group?.tasks.find(t => t.id === taskId)
+		return { board, currentTask: task, groupId: group?.id }
+	}
 
 	const { board, currentTask, groupId } = useSelector(memoizedSelector, shallowEqual)
 
@@ -24,10 +24,12 @@ export function TaskDetailsChecklistItemList({ checklist, onChangeProgress }) {
 
 	useEffect(() => {
 		if (JSON.stringify(checklistForm) !== JSON.stringify(initialChecklistForm)) {
-			const newTask = { ...currentTask, checklists: currentTask.checklists.map(newChecklist => {
-				if (newChecklist.id === checklist.id) return { ...checklist, checkItems: newChecklist.checkItems.map(checkItem => ({ ...checkItem, checked: checklistForm[checkItem.id] })) }
-				return checklist
-			})}
+			const newTask = {
+				...currentTask, checklists: currentTask.checklists.map(newChecklist => {
+					if (newChecklist.id === checklist.id) return { ...checklist, checkItems: newChecklist.checkItems.map(checkItem => ({ ...checkItem, checked: checklistForm[checkItem.id] })) }
+					return checklist
+				})
+			}
 			updateTask(board._id, groupId, newTask)
 			onChangeProgress(Object.values(checklistForm).filter(checked => checked).length)
 		}
@@ -39,8 +41,10 @@ export function TaskDetailsChecklistItemList({ checklist, onChangeProgress }) {
 
 	return (
 		<form onSubmit={ev => ev.preventDefault()} className="task-details-checklist-list" >
-			{checklist.checkItems.map(checkItem => 
-				<TaskDetailsChecklistItemPreview key={checkItem.id} checkItem={checkItem} onChange={ev => onChange(ev)}/>
+			{checklist.checkItems.map(checkItem =>
+				<React.Fragment key={checkItem.id}>
+					{hideCompleted && checkItem.checked ? null : <TaskDetailsChecklistItemPreview key={checkItem.id} checkItem={checkItem} onChange={ev => onChange(ev)}/>}
+				</React.Fragment>
 			)}
 		</form>
 	)
--- src/cmps/task/TaskDetailsChecklistPreview.jsx
+++ src/cmps/task/TaskDetailsChecklistPreview.jsx
@@ -1,26 +1,54 @@
 import { Progress } from "antd";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { TaskDetailsChecklistItemList } from "./TaskDetailsChecklistItemList";
+import { useParams } from "react-router";
+import { shallowEqual, useSelector } from "react-redux";
+import { updateTask } from "../../store/board/board.actions";
 
 export function TaskDetailsChecklistPreview({ checklist }) {
 	const totalItems = checklist.checkItems?.length || 0
-	const [progress, setProgress] = useState(
-		checklist.checkItems?.filter(checkedItem => checkedItem.checked).length / totalItems * 100
-	)
+	const completedItems = checklist.checkItems?.filter(checkedItem => checkedItem.checked).length
+	const [ progress, setProgress ] = useState(completedItems / totalItems * 100)
+	const [ hideCompleted, setHideCompleted ] = useState(false)
+	const hideCompletedText = useRef('Hide checked items')
+	const { taskId } = useParams();
+	const memoizedSelector = (storeState) => {
+        const board = storeState.boardModule.board
+        const group = board.groups.find(g => g.tasks.some(t => t.id === taskId))
+        const task = group?.tasks.find(t => t.id === taskId)
+        return { board, currentTask: task, groupId: group?.id }
+    }
+
+	const { board, currentTask, groupId } = useSelector(memoizedSelector, shallowEqual)
+
+	useEffect(() => {
+		if (hideCompleted) {
+			hideCompletedText.current = `Hide checked items`
+		} else {
+			hideCompletedText.current = `Show checked items (${completedItems})`
+		}
+	}, [hideCompleted])
+
 	function onChangeProgress(completed) {
 		setProgress(totalItems ? completed / totalItems * 100 : 0)
 	}
+
+	function deleteChecklist() {
+		updateTask(board._id, groupId, { ...currentTask, checklists: currentTask.checklists.filter(checklist => checklist.id !== checklist.id) })
+	}
+
 	return (
 		<section className="task-details-checklist task-details-main-item" key={checklist.id}>
 			<section className="task-details-main-item-header">
 				<span className="icon icon-lg icon-checkbox-checked" />
 				<h3>{checklist.title}</h3>
-				<button className="btn">Delete</button>
+				{completedItems > 0 && <button className="btn" onClick={() => setHideCompleted(!hideCompleted)}>{hideCompletedText.current}</button>}
+				<button className="btn" onClick={deleteChecklist}>Delete</button>
 			</section>
 			<section className="task-details-checklist-progress">
 				<Progress percent={progress} percentPosition={{ align: 'start', type: 'outer' }} format={percent => `${percent}%`} />
 			</section>
-			<TaskDetailsChecklistItemList checklist={checklist} onChangeProgress={onChangeProgress} />
+			<TaskDetailsChecklistItemList checklist={checklist} onChangeProgress={onChangeProgress} hideCompleted={hideCompleted} />
 		</section>
 	)
 }
