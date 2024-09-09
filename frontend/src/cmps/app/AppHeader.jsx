5ad217f4b1d7c41f1366d8f798f38cf864f907a9
iriso1 <114158685+iriso1@users.noreply.github.com>
2024-09-10 01:40:21 +0200

    Add create button in header, change labels hiver filter

--- frontend/src/cmps/app/AppHeader.jsx
+++ frontend/src/cmps/app/AppHeader.jsx
@@ -1,6 +1,10 @@
 import { NavLink } from 'react-router-dom'
+import { useMultiPopover } from '../../customHooks/useMultiPopover'
+import { Popover } from 'antd'
+import { BoardAdd } from '../board/BoardAdd'
 
 export function AppHeader() {
+    const { isPopoverOpen, openPopover, closePopover, focusInput, setFocusInput } = useMultiPopover()
 
     return (
         <header className="app-header">
@@ -9,6 +13,20 @@ export function AppHeader() {
                     <div className="logo-img"></div>
                 </NavLink>
                 <NavLink to="/" className="app-header-nav-link btn">Boards</NavLink>
+                <Popover content={props => BoardAdd({ ...props, onClose: closePopover, focusInput, setFocusInput })}
+				open={isPopoverOpen(`popover-add-board`)}
+				onOpenChange={(open) => {
+					if (open) {
+						setFocusInput(true)
+						openPopover(`popover-add-board`)
+					} else {
+						closePopover()
+						setFocusInput(false)
+					}
+				}}
+				placement="bottomLeft" trigger={"click"} arrow={false}>
+				<button className="btn create-board">Create</button>
+			</Popover>
             </nav>
         </header>
     )
